73e6c575abee3c1057b071220bcfe2f4034b0e2c
Will Pittman <willjpittman@gmail.com>
2017-04-17 21:35:07 -0400

    modified sphinx-docs config so that output a little more bearable.

---
+++ docs/make.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+export QT_API=pyside
+export QT_PREFERRED_BINDING=PySide
+
+
+#rm -rf _api
+rm -rf source/_api
+rm -rf build
+env PYTHONPATH=../ sphinx-apidoc --separate -d 5 -o source/_api  ../
+env PYTHONPATH=../ make html
+
